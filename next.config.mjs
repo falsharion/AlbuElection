7c36241285030fbf8354e707283cf83799a05487
falsharion <falomosharl@gmail.com>
2025-03-20 09:46:20 +0100

    production fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    // Prevent CSS minification issues
+    config.optimization.minimizer.forEach((minimizer) => {
+      if (minimizer.constructor.name === 'CssMinimizerPlugin') {
+        minimizer.options.minimizerOptions.preset[1].discardComments = false;
+      }
+    });
+    return config;
+  },
     images: {
 
       remotePatterns: [
